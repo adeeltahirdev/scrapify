fe3fe3ca9483dc5fdfbd2850d47410440c05a8cf
adeeltahirdev <adeeltahir0301@gmail.com>
2026-04-20 21:19:51 +0500

    Extracted name and price from all products

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ ctg_links = ctg.find_elements(By.TAG_NAME, "a")
 
 categories = []
 links = []
+products_data = []
 
 for c in ctg_links:
     ctg_name = c.text.strip()
@@ -64,5 +65,30 @@ for lnks in categories:
 for l in links:
     print(f"Product_Links: {l}")
 print(len(links))
+
+for link in links:
+    driver.get(link)
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, product_name))
+    )
     
+    name = driver.find_element(By.CLASS_NAME, product_name).text
+    
+    sale_element = driver.find_element(By.CLASS_NAME, product_sale_price)
+    
+    if sale_element:
+        original_price = driver.find_element(By.CLASS_NAME, product_original_price).text
+        sale_price = driver.find_element(By.CLASS_NAME, product_sale_price).text
+
+        products_data.append({
+            'Product Name': name,
+            'Original Price': original_price,
+            'Sale Price': sale_price,
+            'Product Link': link
+        })
+        
+for data in products_data:
+    print(data)
+print(len(products_data))
+        
 driver.quit()
