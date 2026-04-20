767a9e6176049604fe690511ccc8216e0605d715
adeeltahirdev <adeeltahir0301@gmail.com>
2026-04-20 20:44:20 +0500

    Extracted categories and then extracted product links

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ driver.get("https://radstore.pk/")
 shops = "site-nav__link"
 product = "grid-product__link"
 product_name = "product-single__title"
-product_price = "product__price"
 product_original_price = "product__price--compare"
 product_sale_price = "sale-price"
+product_price = "product__price"
+ul_tag = "tags--vertical"
 
 WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.CLASS_NAME, shops))
@@ -26,53 +27,42 @@ WebDriverWait(driver, 5).until(
 shop = driver.find_element(By.CLASS_NAME, shops)
 shop.click()
 
-products = WebDriverWait(driver, 20).until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, product))
+ctg = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, ul_tag))
 )
 
-i = 1
 
+ctg_links = ctg.find_elements(By.TAG_NAME, "a")
+
+
+categories = []
 links = []
-products_data = []
 
-for p in products:
-    href = p.get_attribute("href")
-    links.append(href)
-    i += 1
+for c in ctg_links:
+    ctg_name = c.text.strip()
+    ctg_href = c.get_attribute("href")
+    categories.append({
+        'Category': ctg_name,
+        'Link': ctg_href
+    })
     
+print(len(categories))
+for ct in categories:
+    print(ct)
+
+for lnks in categories:
+    driver.get(lnks['Link'])
+    
+    products = WebDriverWait(driver, 20).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, product))
+    )
+    
+    for p in products:
+        product_href = p.get_attribute("href")
+        links.append(product_href)
+
+for l in links:
+    print(f"Product_Links: {l}")
 print(len(links))
-
-for link in links:
-    driver.get(link)
-    WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, product_name))
-    )
-    WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, product_sale_price))
-    )
-    name = driver.find_element(By.CLASS_NAME, product_name).text
-    if not product_sale_price:
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, product_price))
-        )
-        price = driver.find_element(By.CLASS_NAME, product_price).text
-        products_data.append({'Name': name, 'Price': price, 'URL': link})
-    else:
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, product_sale_price))
-        )
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CLASS_NAME, product_original_price))
-        )
-        original_price = driver.find_element(By.CLASS_NAME, product_original_price).text
-        sale_price = driver.find_element(By.CLASS_NAME, product_sale_price).text
-
-        products_data.append({'Name': name, 'Original Price': original_price, 'Sale Price': sale_price, 'URL': link})
     
-
-for data in products_data:
-    print(data)
-    
-print(len(products_data))
-
 driver.quit()
