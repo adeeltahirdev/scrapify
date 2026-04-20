4aa06436e502f6a41ddc58433819c2a6a3f4a0a8
adeeltahirdev <adeeltahir0301@gmail.com>
2026-04-20 21:49:42 +0500

    Refactored the price logic

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ print(len(categories))
 for ct in categories:
     print(ct)
 
-for lnks in categories:
-    driver.get(lnks['Link'])
+for item in categories:
+    driver.get(item['Link'])
     
     products = WebDriverWait(driver, 20).until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, product))
@@ -60,32 +60,48 @@ for lnks in categories:
     
     for p in products:
         product_href = p.get_attribute("href")
-        links.append(product_href)
+        links.append({
+            'URL': product_href,
+            'Category': item['Category']
+        })
 
 for l in links:
     print(f"Product_Links: {l}")
 print(len(links))
 
-for link in links:
-    driver.get(link)
+for items in links:
+    driver.get(items['URL'])
     WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.CLASS_NAME, product_name))
     )
     
     name = driver.find_element(By.CLASS_NAME, product_name).text
     
-    sale_element = driver.find_element(By.CLASS_NAME, product_sale_price)
-    
-    if sale_element:
-        original_price = driver.find_element(By.CLASS_NAME, product_original_price).text
-        sale_price = driver.find_element(By.CLASS_NAME, product_sale_price).text
+    sale_elements = driver.find_elements(By.CLASS_NAME, product_sale_price)
 
-        products_data.append({
-            'Product Name': name,
-            'Original Price': original_price,
-            'Sale Price': sale_price,
-            'Product Link': link
-        })
+    if sale_elements:
+        
+        sale_price = driver.find_element(By.CLASS_NAME, product_sale_price).text
+        original_elements = driver.find_elements(By.CLASS_NAME, product_original_price)
+
+        original_price = original_elements[0].text if original_elements else None
+
+        final_price = sale_price
+    else:
+        
+        price = driver.find_element(By.CLASS_NAME, product_price).text
+
+        final_price = price
+        original_price = None
+
+
+    products_data.append({
+        'name': name,
+        'price': final_price,
+        'original_price': original_price,
+        'url': items['URL'],
+        'category': items['Category']
+    })
         
 for data in products_data:
     print(data)
