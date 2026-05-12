20668834f74c6edb6e5e3dccfe7229ed15c5aca8
adeeltahirdev <adeeltahir0301@gmail.com>
2026-05-12 19:44:45 +0500

    Integerated database to the second site

--- scrapers/surteez.py
+++ scrapers/surteez.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from model import get_product_by_url, insert_product, update_product, insert_price, insert_variant, delete_variants
 import time
 import random
 
@@ -24,8 +25,6 @@ def scrape_surteez():
     category = "list-collections-section__container"
     product_link_class = "product__media__holder"
     product_name = "product__title"
-    sale = "product__price--regular"
-    orgnl_price = "product__price--compare"
     variant = ".select-popout__option"
     description_class = "product__description"
 
@@ -134,7 +133,7 @@ def scrape_surteez():
                 stock_status = "in stock"
                 
             variants.append({
-                'Variant': variant_name,
+                'variant': variant_name,
                 'stock_status': stock_status
             })
             
@@ -149,6 +148,23 @@ def scrape_surteez():
             'Variants': variants,
             'Description': description
         })
+        
+        existing_product = get_product_by_url(item['URL'])
+        
+        if existing_product:
+            product_id = existing_product[0]
+            update_product(product_id, name, description, item['Category'])
+            insert_price(product_id, sale_price, original_price)
+            delete_variants(product_id)
+            
+            for var in variants:
+                insert_variant(product_id, var['variant'], var['stock_status'])
+        else:
+            product_id = insert_product(name, item['URL'], description, item['Category'])
+            insert_price(product_id, sale_price, original_price)
+            
+            for var in variants:
+                insert_variant(product_id, var['variant'], var['stock_status'])
         
     for c in categories:
         print(c)
