8248b1dba960d64edacad9b7c8251310008bc9e2
adeeltahirdev <adeeltahir0301@gmail.com>
2026-05-13 21:31:20 +0500

    Extracted products detail

--- scrapers/konfor.py
+++ scrapers/konfor.py
@@ -20,7 +20,7 @@ def scrape_konfor():
     
     
     category_id = "t4s-nav-ul"
-    product_link_class = "t4s-full-width-link"
+    product_name = "t4s-product__title"
     
     WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.ID, category_id))
@@ -32,6 +32,7 @@ def scrape_konfor():
     
     categories = []
     links = []
+    product_data = []
     
     for item in category_links[:8]:
         category_name = item.get_attribute("textContent").strip()
@@ -58,6 +59,52 @@ def scrape_konfor():
                 'Category': link['Category']
             })
     
+    for item in links:
+        driver.get(item['URL'])
+        
+        time.sleep(random.uniform(2, 4))
+        
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.CLASS_NAME, product_name))
+        )
+        
+        name = driver.find_element(By.CLASS_NAME, product_name).text
+        
+        sale_price = driver.find_element(By.CSS_SELECTOR, "div.t4s-product-price ins").text
+        
+        original_price = driver.find_element(By.CSS_SELECTOR, "div.t4s-product-price del").text
+        
+        variant_elements = driver.find_elements(By.CSS_SELECTOR, '[data-swatch-item]')
+
+        variants = []
+
+        for v in variant_elements:
+
+            variant_name = v.get_attribute("data-value")
+            classes = v.get_attribute("class")
+
+            if "is--soldout" in classes:
+                stock_status = "out of stock"
+            else:
+                stock_status = "in stock"
+
+            variants.append({
+                "variant": variant_name,
+                "stock_status": stock_status
+            })
+
+        description = driver.find_element(By.CSS_SELECTOR, "div.t4s-rte.t4s-tab-content").text
+        
+        product_data.append({
+            'Name': name,
+            'URL': item['URL'],
+            'Category': item['Category'],
+            'Price': sale_price,
+            'Original Price': original_price,
+            'Variants': variants,
+            'Description': description
+        })
+    
     for c in categories:
         print(c)
     print(f'Total Categories: {len(categories)}')
@@ -65,5 +112,9 @@ def scrape_konfor():
     for l in links:
         print(l)
     print(f'Total Product Links: {len(links)}')
+    
+    for p in product_data:
+        print(p)
+    print(f'Total Products: {len(product_data)}')
     
     driver.quit()
